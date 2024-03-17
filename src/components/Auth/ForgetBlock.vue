b38f97b6d3927890043ef89c059b9de24bf3ff8e
linxin4cs <linxin4cs@gmail.com>
2024-03-17 11:28:07 +0800

    feat: 修改 忘记密码步骤条图标

--- src/components/Auth/ForgetBlock.vue
+++ src/components/Auth/ForgetBlock.vue
@@ -1,114 +1,106 @@
 <script setup>
-import { reactive, ref } from "vue";
-import { EditPen, Message, Lock } from "@element-plus/icons-vue";
-import { ElMessage } from "element-plus";
+import { reactive, ref } from 'vue'
+import { EditPen, Message, Lock } from '@element-plus/icons-vue'
+import { ElMessage } from 'element-plus'
 
-const props = defineProps(["pageMode"]);
-const pageMode = props.pageMode;
+const props = defineProps(['pageMode'])
+const pageMode = props.pageMode
 
-const active = ref(0);
-const isEmailValid = ref(false);
-const coldDownTime = ref(0);
+const active = ref(0)
+const isEmailValid = ref(false)
+const coldDownTime = ref(0)
 
-const formRef1 = ref();
-const formRef2 = ref();
+const formRef1 = ref()
+const formRef2 = ref()
 
 const form = reactive({
-	email: "",
-	password: "",
-	passwordAgain: "",
-	code: "",
-});
+	email: '',
+	password: '',
+	passwordAgain: '',
+	code: ''
+})
 
 const ValidatePasswordAgain = (rule, value, callback) => {
-	if (value === "") {
-		callback(new Error("请再次输入密码"));
+	if (value === '') {
+		callback(new Error('请再次输入密码'))
 	} else if (value !== form.password) {
-		callback(new Error("两次输入密码不一致"));
+		callback(new Error('两次输入密码不一致'))
 	} else {
-		callback();
+		callback()
 	}
-};
+}
 
 const rules = {
 	email: [
 		{
 			required: true,
-			message: "请输入邮箱地址",
-			trigger: ["blur", "change"],
+			message: '请输入邮箱地址',
+			trigger: ['blur', 'change']
 		},
 		{
-			type: "email",
-			message: "请输入合法的邮件地址",
-			trigger: ["blur", "change"],
-		},
+			type: 'email',
+			message: '请输入合法的邮件地址',
+			trigger: ['blur', 'change']
+		}
 	],
 	code: [
 		{
 			required: true,
-			message: "请输入验证码",
-			trigger: ["blur", "change"],
-		},
+			message: '请输入验证码',
+			trigger: ['blur', 'change']
+		}
 	],
 	password: [
-		{ required: true, message: "请输入密码", trigger: ["change", "blur"] },
+		{ required: true, message: '请输入密码', trigger: ['change', 'blur'] },
 		{
 			min: 6,
 			max: 16,
-			message: "长度只能在 6 到 16 之间",
-			trigger: ["blur", "change"],
-		},
+			message: '长度只能在 6 到 16 之间',
+			trigger: ['blur', 'change']
+		}
 	],
-	passwordAgain: [
-		{ validator: ValidatePasswordAgain, trigger: ["blur", "change"] },
-	],
-};
+	passwordAgain: [{ validator: ValidatePasswordAgain, trigger: ['blur', 'change'] }]
+}
 
 const onValidate = (prop, isValid) => {
-	if (prop === "email") {
-		isEmailValid.value = isValid;
+	if (prop === 'email') {
+		isEmailValid.value = isValid
 	}
-};
+}
 
-const validateEmail = () => {};
+const validateEmail = () => {}
 
 const startReset = () => {
 	formRef1.value.validate((valid) => {
 		if (valid) {
 			/* empty */
 		} else {
-			ElMessage.warning("请填写完整信息");
+			ElMessage.warning('请填写完整信息')
 		}
-	});
-};
+	})
+}
 
 const doReset = () => {
 	formRef2.value.validate((valid) => {
 		if (valid) {
 			/* empty */
 		} else {
-			ElMessage.warning("请填写完整信息");
+			ElMessage.warning('请填写完整信息')
 		}
-	});
-};
+	})
+}
 </script>
 
 <template>
 	<div>
 		<el-steps :active="active" finish-status="success" align-center>
-			<el-step title="验证电子邮件" />
-			<el-step title="重新设定密码" />
+			<el-step title="验证电子邮件" :icon="Message" />
+			<el-step title="重新设定密码" :icon="Lock" />
 		</el-steps>
 		<transition name="el-fade-in-linear" mode="out-in">
 			<div v-if="active === 0">
 				<div class="mt-2 mb-6 text-2xl">重置密码</div>
-				<el-form
-					:model="form"
-					:rules="rules"
-					@validate="onValidate"
-					ref="formRef1"
-					class="w-72"
-				>
+				<el-form :model="form" :rules="rules" @validate="onValidate" ref="formRef1" class="w-72">
 					<el-form-item prop="email">
 						<el-input v-model="form.email" placeholder="邮箱" type="text">
 							<template #prefix>
@@ -121,12 +113,7 @@ const doReset = () => {
 					<el-form-item prop="code">
 						<el-row :gutter="10">
 							<el-col :span="17">
-								<el-input
-									maxlength="6"
-									v-model="form.code"
-									placeholder="验证码"
-									type="text"
-								>
+								<el-input maxlength="6" v-model="form.code" placeholder="验证码" type="text">
 									<template #prefix>
 										<el-icon>
 											<EditPen />
@@ -139,11 +126,7 @@ const doReset = () => {
 									@click="validateEmail"
 									color="#67c23aff"
 									:disabled="!isEmailValid || coldDownTime > 0"
-									>{{
-										coldDownTime > 0
-											? "请稍后 " + coldDownTime + "秒"
-											: "获取验证码"
-									}}
+									>{{ coldDownTime > 0 ? '请稍后 ' + coldDownTime + '秒' : '获取验证码' }}
 								</el-button>
 							</el-col>
 						</el-row>
@@ -155,9 +138,7 @@ const doReset = () => {
 				<el-divider>
 					<span class="text-gray-400 text-xs">点错了？</span>
 				</el-divider>
-				<router-link
-					:to="pageMode === 'admin' ? '/admin/auth/login' : '/auth/login'"
-				>
+				<router-link :to="pageMode === 'admin' ? '/admin/auth/login' : '/auth/login'">
 					<el-button class="w-28" color="#67c23aff" plain>去登录 </el-button>
 				</router-link>
 			</div>
@@ -165,23 +146,10 @@ const doReset = () => {
 		<transition name="el-fade-in-linear" mode="out-in">
 			<div v-if="active === 1">
 				<div class="mt-2 mb-1 text-2xl">重置密码</div>
-				<div class="text-sm text-gray-400 mb-6">
-					请填写您的新密码，务必牢记，防止丢失
-				</div>
-				<el-form
-					:model="form"
-					:rules="rules"
-					@validate="onValidate"
-					class="w-72"
-					ref="formRef2"
-				>
+				<div class="text-sm text-gray-400 mb-6">请填写您的新密码，务必牢记，防止丢失</div>
+				<el-form :model="form" :rules="rules" @validate="onValidate" class="w-72" ref="formRef2">
 					<el-form-item prop="password">
-						<el-input
-							:maxlength="16"
-							v-model="form.password"
-							placeholder="密码"
-							type="password"
-						>
+						<el-input :maxlength="16" v-model="form.password" placeholder="密码" type="password">
 							<template #prefix>
 								<el-icon>
 									<Lock />
@@ -204,15 +172,11 @@ const doReset = () => {
 						</el-input>
 					</el-form-item>
 				</el-form>
-				<el-button @click="doReset" class="w-28" type="danger" plain
-					>立即重置密码
-				</el-button>
+				<el-button @click="doReset" class="w-28" type="danger" plain>立即重置密码 </el-button>
 				<el-divider>
 					<span class="text-gray-400 text-xs">点错了？</span>
 				</el-divider>
-				<router-link
-					:to="pageMode === 'admin' ? '/admin/auth/login' : '/auth/login'"
-				>
+				<router-link :to="pageMode === 'admin' ? '/admin/auth/login' : '/auth/login'">
 					<el-button class="w-28" color="#67c23aff" plain>去登录 </el-button>
 				</router-link>
 			</div>
